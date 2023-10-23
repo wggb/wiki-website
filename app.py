b6c88cab562ea2f5f200eda8c88aae47252bd4c5
Sadra Saderi <sadrassx@gmail.com>
2023-10-24 00:24:17 +0330

    Enable debug mode and disable caching

--- app.py
+++ app.py
@@ -9,35 +9,49 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    response.headers["Cache-Control"] = "public, max-age=0"
+    return response
+
+
 # configure SQLAlchemy to use db
 # todo
+
 
 @app.route("/", methods=["GET"])
 # requite login
 # todo
 def homepage():
-
     if session.get("user_id") is None:
-            return redirect("/login")
-    
-    return render_template('homepage.html')
+        return redirect("/login")
 
-@app.route('/login', methods=["GET", "POST"])
+    return render_template("homepage.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-
     session.clear()
-    if request.method == 'POST':
+    if request.method == "POST":
         if not request.form.get("username"):
-            return render_template('apology.html')
+            return render_template("apology.html")
         if not request.form.get("password"):
-            return render_template('apology.html')
+            return render_template("apology.html")
 
         # todo: check database for user
 
         # todo: start session
 
         # if all is right
-        return redirect('/')
-    
+        return redirect("/")
+
     else:
-        return render_template('login.html')
+        return render_template("login.html")
