592689da4cded0a299fc7ad178bd05655276dcfb
Alireza Mosadegh <alirezampv21@gmail.com>
2023-12-12 20:49:28 +0330

    implement markdown table
    also cleaned up a bit

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import CHAR, Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy import CHAR, Column, ForeignKey, Integer, String, Text, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -12,28 +12,17 @@ class User(Base):
     username = Column(String)
     password = Column(String)
 
-    # def __init__(self, id, username, password):
-    #     self.id = id
-    #     self.username = username
-    #     self.password = password
 
-    # def __repr__(self):
-    #     return f" {self.id}, {self.username}, {self.password} "
+class MarkdownFile(Base):
+    __tablename__ = "markdown_files"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(Text)
 
 
-engine = create_engine("sqlite:///db.sqlite", echo=True)  # makes database in memory
-Base.metadata.create_all(
-    bind=engine
-)  # takes all the classes that extend from base and creates them in the database
+engine = create_engine("sqlite:///db.sqlite", echo=True)
+Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
-
-# in use:
-# user1 = User(123, "Bob", "123456")
-# session.add(user1)  # with this line of code this user will be created in the database
-# session.commit()
-
-# this would be SELECT * FROM users
-# data = session.query(User).all()
-# print(data)
